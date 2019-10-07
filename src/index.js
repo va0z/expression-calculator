da1eb78c6b7f1a972393785421f51bdbbf2f1d9f
va0z <va0z@ya.ru>
2019-10-07 18:18:33 +1000

    init: start

--- src/index.js
+++ src/index.js
@@ -5,6 +5,31 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    var res;
+    var str = expr.replace(/ /g, "");
+    console.log(str);
+    for (i=0; i<str.length; i++) {
+        // console.log("i: " + str[i]);
+        if (str[i]=="+") { 
+            // console.log("i: " + str[i]);
+            res=Number.parseInt(str[i-1]) + Number.parseInt(str[i+1]);
+        }
+        if (str[i]=="-") { 
+            // console.log("i: " + str[i]);
+            res=Number.parseInt(str[i-1]) - Number.parseInt(str[i+1]);
+        }
+        if (str[i]=="*") { 
+            // console.log("i: " + str[i]);
+            res=Number.parseInt(str[i-1]) * Number.parseInt(str[i+1]);
+        }
+        if (str[i]=="/") { 
+            // console.log("i: " + str[i]);
+            // test division by zero
+            res=Number.parseInt(str[i-1]) / Number.parseInt(str[i+1]);
+        }
+    }
+    console.log(res);
+    return (res);
 }
 
 module.exports = {
